dca77f60a0087a3af942e66611bf3123f778a3a8
Sushen Kumar <sushenvkumar@gmail.com>
2025-02-20 02:00:05 -0600

    In-place optimal solution

---
+++ 00027-remove-element.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i, k = 0, 0
+        while i < len(nums):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                i += 1
+                k += 1
+            else:
+                i += 1
+        print(k, nums)
+        return k
+
+nums, val = [2, 2, 2, 8, 9, 7, 7, 7], 2
+s = Solution()
+s.removeElement(nums, val)
