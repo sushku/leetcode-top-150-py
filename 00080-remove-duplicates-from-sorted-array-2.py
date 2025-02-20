bbe0b71c5926375b00b0d29f52382ee783d806a9
Sushen Kumar <sushenvkumar@gmail.com>
2025-02-20 12:49:38 -0600

    Remove dup 2 solution

---
+++ 00080-remove-duplicates-from-sorted-array-2.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i, k = 0, 0
+        prevNum = "Not set"
+        while i < len(nums):
+            if prevNum == "Not set":
+                prevNum = nums[i]
+                nums[k] = nums[i]
+                count = 1
+                i += 1
+                k += 1
+                continue
+            if nums[i] == prevNum and count < 2:
+                nums[k] = nums[i]
+                count += 1
+                i += 1
+                k += 1
+            elif nums[i] != prevNum:
+                nums[k] = nums[i]
+                prevNum = nums[i]
+                count = 1
+                i += 1
+                k += 1
+            else:
+                i += 1
+        return k
+
+nums = [-1, 0, 0, 0, 1, 1, 2, 2, 2, 2, 3, 4, 4, 4, 5, 5]
+s = Solution()
+s.removeDuplicates(nums)
