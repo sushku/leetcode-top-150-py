640e7be62d9343dcfd92cf78fc97f2748e6bebed
Sushen Kumar <sushenvkumar@gmail.com>
2025-02-26 00:02:46 -0600

    Length of last word

---
+++ 00058-length-of-last-word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        words = s.split()
+        return len(words[-1]) if words else 0
+
+sentence = "   we  "
+s = Solution()
+print(s.lengthOfLastWord(sentence))
